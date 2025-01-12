e034c2de25e264090e48ec872e66aff3dae640cc
yklee <gerion@gmail.com>
2025-01-12 12:38:58 +0900

    컨텐츠 스크롤 처리를 위한 샘플추가

--- src/components/ChildComp1.vue
+++ src/components/ChildComp1.vue
@@ -18,6 +18,20 @@ function callParent() {
     <p>Child Comp1-1</p>
     <p>Child Comp1-2</p>
     <p>Child Comp1-3</p>
+    <p>Child Comp1-4</p>
+    <p>Child Comp1-5</p>
+    <p>Child Comp1-6</p>
+    <p>Child Comp1-7</p>
+    <p>Child Comp1-8</p>
+    <p>Child Comp1-9</p>
+    <p>Child Comp1-10</p>
+    <p>Child Comp1-11</p>
+    <p>Child Comp1-12</p>
+    <p>Child Comp1-13</p>
+    <p>Child Comp1-14</p>
+    <p>Child Comp1-15</p>
+    <p>Child Comp1-16</p>
+    <p>Child Comp1-17</p>
     <div>
       <button @click="callParent">emit</button>
     </div>
