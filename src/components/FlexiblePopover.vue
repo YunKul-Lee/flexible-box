c52ca2635b95a6aecefee6a2f41cb395674bebfb
yklee <gerion@gmail.com>
2025-01-18 19:30:34 +0900

    resizer 영역 활성화

--- src/components/FlexiblePopover.vue
+++ src/components/FlexiblePopover.vue
@@ -131,8 +131,8 @@ function updateData(...params: any[]) {
   width: 10px;
   height: 10px;
   /* background: white; */
-  /* border: 3px solid #4286f4; */
-  background: rgba(255,255,255, 0);
+  border: 3px solid #4286f4;
+  /* background: rgba(255,255,255, 0); */
   position: absolute;
 }
 
